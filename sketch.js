3c78479be93ffde11eac756f8c9c21756eff0afc
KaeshaTalreja <65214092+KaeshaTalreja@users.noreply.github.com>
2020-07-09 16:56:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -52,32 +52,5 @@ function draw() {
     rect1.shapeColor="green"
   }
 
-  
-
   drawSprites();
-}
-//function which can accept input/arguments
-function isTouching(object1,object2){
-  if (object2.x - object1.x < object1.width/2 + object2.width/2
-      && object1.x - object2.x < object1.width/2 + object2.width/2
-      && object2.y - object1.y < object1.height/2 + object2.height/2
-      && object1.y - object2.y < object1.height/2 + object2.height/2) {
-    return true;
-  }
-  else {
-    return false;
-  }
-}
-
-function bounceOff(){
-  if (movingRect.x - fixedRect.x < fixedRect.width/2 + movingRect.width/2
-      && fixedRect.x - movingRect.x < fixedRect.width/2 + movingRect.width/2) {
-    movingRect.velocityX=movingRect.velocityX * (-1);
-    fixedRect.velocityX *= (-1);
-  }
-  if(movingRect.y - fixedRect.y < fixedRect.height/2 + movingRect.height/2
-      && fixedRect.y - movingRect.y < fixedRect.height/2 + movingRect.height/2) {
-    movingRect.velocityY=movingRect.velocityY * (-1);
-    fixedRect.velocityY *= (-1);
-  }
 }
